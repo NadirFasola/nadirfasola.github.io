65a73209e657f75c4d370344c617bcfb3f660c27
Nadir Fasola <nadir.fasola@gmail.com>
2022-03-17 11:02:59 +0000

    Added lazy loading. Fixed minor issues.

--- assets/js/createGallery.js
+++ assets/js/createGallery.js
@@ -7,15 +7,15 @@ let modalClose = document.getElementById("modal-close-btn");
 let galleryItems = document.querySelectorAll(".gallery-item");
 
 const openModal = (e) => {
-  console.log(e.target.parentElement.getAttribute("data-fullSizeJpg"));
-  modalImage.innerHTML = `<source class="modal-webp" srcset=${e.target.parentElement.getAttribute(
+  console.log(
+    e.target.parentElement.parentElement.getAttribute("data-fullSizeJpg")
+  );
+  modalImage.innerHTML = `<source class="modal-webp" srcset=${e.target.parentElement.parentElement.getAttribute(
     "data-fullSizeWebp"
-  )} /><source class="modal-jpg" srcset=${e.target.parentElement.getAttribute(
-    "data-fullSizeJpg"
-  )} /><img src=${e.target.parentElement.getAttribute(
+  )} type="image/webp" /><img src=${e.target.parentElement.parentElement.getAttribute(
     "data-fullSizeJpg"
   )} alt=${e.target.alt} />`;
-  modalCaption.innerHTML = `<p>${e.target.parentElement.getAttribute(
+  modalCaption.innerHTML = `<p>${e.target.parentElement.parentElement.getAttribute(
     "data-caption"
   )}</p>`;
   modalContainer.style.display = "block";
@@ -24,6 +24,18 @@ const openModal = (e) => {
       (window.innerHeight - pictureCaptionWrapper.offsetHeight) / 2
     }px)`;
   };
+};
+
+const lazyLoad = (pic) => {
+  const img = pic.querySelector("img");
+  const source = pic.querySelector("source");
+
+  source.srcset = source.getAttribute("data-srcset");
+  source.removeAttribute("data-srcset");
+  img.src = img.getAttribute("data-src");
+  img.removeAttribute("data-src");
+
+  img.classList.remove("placeholder");
 };
 
 galleryItems.forEach((item) => {
@@ -41,3 +53,21 @@ window.onresize = () => {
     (window.innerHeight - pictureCaptionWrapper.offsetHeight) / 2
   }px)`;
 };
+
+const observerConfig = {
+  root: null,
+  rootMargin: "600px",
+};
+
+const imgObserver = new IntersectionObserver((entries, self) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      lazyLoad(entry.target);
+      self.unobserve(entry.target);
+    }
+  });
+}, observerConfig);
+
+document.querySelectorAll(".lazy-picture").forEach((pic) => {
+  imgObserver.observe(pic);
+});
